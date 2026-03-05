2808011ca03284c2c14abc839314522a35326fe1
AnnOkk <chereshnyasladkaya@gmail.com>
2026-03-05 14:00:03 +0200

    First

---
+++ http-dz.js
@@ -0,0 +1,26 @@
+import http from 'node:http'
+import dotenv from 'dotenv'
+import * as url from "node:url"; //!
+
+dotenv.config() // возьми файл дотэнв и используй переменную оттуда(значение)
+
+const port = process.env.port || 3000;
+
+const server = http.createServer((req, res) => {
+
+    // let url = new URL(req.url,'http://localhost');
+    let url = req.url;
+    const name = url.searchParams.get('name');
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'})
+    if (req.url) {
+        res.write(`<h1> Hello ${name}!</h1>`)
+    }
+    res.end();
+})
+
+
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}. Press Ctrl+C to quit`)
+})
+
