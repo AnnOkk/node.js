34919f1f4e9205cb27835f31185995ad1f856e5c
edward <edwardlfish@gmail.com>
2026-03-13 09:03:27 +0200

    Add new student routes and controllers for scores, names, and minimum score filters

--- src/controller/studentController.js
+++ src/controller/studentController.js
@@ -38,3 +38,31 @@ export const updateStudent = (req, res) => {
         res.status(404).send();
     }
 }
+
+export const addScore = (req, res) => {
+    const success = repo.addScore(+req.params.id, req.body.examName, +req.body.score);
+    if (success) {
+        res.status(204).send();
+    } else {
+        res.status(404).send();
+    }
+}
+
+export const findByName = (req, res) => {
+    const students = repo.findByName(req.params.name);
+    const studentsWithoutPasswords = students.map(student => ({...student, password: undefined}))
+    res.json(studentsWithoutPasswords);
+}
+
+export const countByNames = (req, res) => {
+    const names = req.query.names;
+    const list = Array.isArray(names) ? names : [names];
+    const count = repo.countByNames(list);
+    res.json(count)
+}
+
+export const findByMinScore = (req, res) => {
+    const students = repo.findByMinScore(req.params.exam, +req.params.minScore);
+    const studentsWithoutPasswords = students.map(student => ({...student, password: undefined}))
+    res.json(studentsWithoutPasswords);
+}
--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
-import {addStudent, deleteStudent, findStudent, updateStudent} from "../controller/studentController.js";
+import {
+    addScore,
+    addStudent, countByNames,
+    deleteStudent, findByMinScore,
+    findByName,
+    findStudent,
+    updateStudent
+} from "../controller/studentController.js";
 
 const router = Router();
 
@@ -7,5 +14,9 @@ router.post('/student', addStudent);
 router.get('/student/:id', findStudent);
 router.delete('/student/:id', deleteStudent);
 router.patch('/student/:id', updateStudent);
+router.patch('/score/student/:id', addScore);
+router.get('/students/name/:name', findByName);
+router.get('/quantity/students', countByNames);
+router.get('/students/exam/:exam/minscore/:minScore', findByMinScore);
 
 export default router;
